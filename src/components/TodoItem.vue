a2452c3bf45f28ec3eb22e9461a001248acb4150
Fally Christian <se231313@fhstp.ac.at>
2026-09-18 16:30:00 +0200

    260918 - Todo Item

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,9 +1,32 @@
 <script setup lang="ts">
+import type { Todo } from '../types'
+
+const props = defineProps<{
+  todo: Todo
+}>()
+
+const emit = defineEmits<{
+  toggle: [id: number]
+  delete: [id: number]
+}>()
+
+function handleToggle() {
+  emit('toggle', props.todo.id)
+}
+
+function handleDelete() {
+  emit('delete', props.todo.id)
+}
+
 
 </script>
 
 <template>
-
+  <div>
+    {{ todo.text }}
+    <input :checked="todo.done" type="checkbox" @change="handleToggle"/>
+    <button @click="handleDelete">Löschen</button>
+  </div>
 </template>
 
 <style scoped>
